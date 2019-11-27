85e1c6847f713760532766e1f2f6968a90430c64
KOlia <m.bondarcul93@gmail.com>
2019-11-27 17:44:42 +0200

    test

--- test/specs/smoke.js
+++ test/specs/smoke.js
@@ -1,10 +1,10 @@
+const assert = require('assert');
 
-describe('Website', function(){
-    it('should be alive', function() {
-        browser.url(`/`)
-        const img = $('img[src="http://ip-5236.sunline.net.ua:38015/images/logotype.png"]')
-        if(!img.isExisting()) {
-            throw new Error('Website should be opened, and logo');
-        }
-    })
-}
+describe('Website', function () {
+it('should be alive', function () {
+browser.url('/')
+const img = $('img[src="http://ip-5236.sunline.net.ua:38015/images/logotype.png"]')
+assert(img.isExisting(), 'Website should be opened, and logo displayed')
+browser.pause(5000)
+})
+})
