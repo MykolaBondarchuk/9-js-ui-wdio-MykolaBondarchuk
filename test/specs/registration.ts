35db00c5b60e8ff10c6f0ea0733558fe1cd0961f
KOlia <m.bondarcul93@gmail.com>
2019-12-16 21:56:34 +0200

    test

--- test/specs/registration.ts
+++ test/specs/registration.ts
@@ -1,5 +1,7 @@
 import * as faker from 'faker';
 
+import * as assert from 'assert';
+
 describe('User', function() {
     it('can register', function() {
        browser.url(`/create_account`)
@@ -17,6 +19,14 @@ describe('User', function() {
        $(".row input[name='password']").setValue(randomEmail)
        $('input[name="confirmed_password"]').setValue(randomEmail)
        $("button[name='create_account']").click()
+
+       browser.pause(2000)
+
+       const alert = $('#notices .alert-success')
+
+       assert(alert.isDisplayed(), `Expected success alert`)
+       
+
 
        browser.debug();
 
